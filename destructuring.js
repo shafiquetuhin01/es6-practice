76262787460d1eb3a6fc6a0ade4637a8ee907a90
Shafique <sfqtuhin04@gmail.com>
2022-02-22 19:36:11 +0600

    es6 added

---
+++ destructuring.js
@@ -0,0 +1,3 @@
+const myObj = { name: "Tuhin", age: 36, wife: "Hosna", class: "second", location: "Dhaka", area: "Air HQ" };
+const { area, age } = myObj;
+console.log(age, area);
---
+++ es6.js
@@ -0,0 +1,41 @@
+/* const aa = "ami jani ami kothay jassi";
+const literal = `
+    Hi Shafique, How are you?
+    You are a good and honest man.
+    If you know that you are ${5 + 88} ${aa} the best in your society then you are the wrong path.
+    Your desires are well known.
+    Your crativity will show the system.
+    
+    Thank you.
+`;
+
+console.log(literal);
+ */
+/* function aa(a, b) {
+    let add = a * b;
+    return add;
+}
+const c = aa(23, 42);
+console.log(c);
+
+function et(a, b = 24, c = 24) {
+    let rr = a * b - c;
+    return rr;
+}
+const dfs = et(23);
+console.log(dfs); */
+
+const sum = a => a;
+const aa = sum(23);
+console.log(aa);
+
+const sf = (e, s, c) => {
+    let sdf = 23;
+    let wer = 344;
+    let dg = 343;
+    let sff = wer + sdf + dg;
+    return sff;
+}
+const df = sf();
+console.log(df);
+console.log(object);
