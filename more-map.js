12a5171f916e5e5c9abfe101c3402a9fd1fb76c2
BiasDesk1 <shafiquetuhin01>
2022-02-22 10:33:58 +0600

    practicing es6

---
+++ more-map.js
@@ -0,0 +1,3 @@
+const lenght = ['Mohammad Shafiqul Islam','Mohammad Shahid Miah','Mosammat Hasina Begum', 'Mohammad Shariful Islam', 'Mosammat Maksuda Akter','Hosna Akter','Nusair Bin Shafique','Nahian Bin Shafique'];
+const nameLenght = lenght.map(x => x.length);
+console.log(nameLenght);
