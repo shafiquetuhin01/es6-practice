962b97f795d8af3e9b2d0a4e46532e8d3adf7960
Shafique <sfqtuhin04@gmail.com>
2022-02-22 22:31:59 +0600

    es6 cleared

---
+++ test.js
@@ -0,0 +1,22 @@
+// 1
+/* let ami = "Amar sonar bangla";
+const amar = "This is Shafique"; */
+/* // ami = "amar";
+// console.log(ami);
+// 2
+const myObj = { name: "Tuhin", age: 37 }
+const deploy = `
+    ${(ami)} ami tumay balobashi, chirodin tomar akash tumar batash amar prane bajay bashi.
+    My name is Md Shafique ${myObj.name} , my age is ${myObj.age} .
+`;
+console.log(deploy); */
+// 3
+/* const arrFn = x => x * 5;
+const result = arrFn(8);
+console.log(result); */
+// 4
+const arrFn = (x, y) => (x, y) + 2 * (x, y);
+const add = arrFn(10, 20);
+const multiply = arrFn(10, 20);
+const divide = arrFn(10, 20);
+console.log(add);
